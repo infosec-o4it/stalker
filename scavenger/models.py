baa858640eeba25ae90ad80b246e95e5268ca62c
arincon@o4it.com <arincon@o4it.com>
2014-12-09 19:19:01 -0500

    se agregaron los modelos para manejo de cliente.

--- scavenger/models.py
+++ scavenger/models.py
@@ -80,3 +80,116 @@ class SistemaOperativo(models.Model):
 
     def __unicode__(self):
         return u"Sistema Operativo %s" % (self.Nombre)
+
+
+class ArquitecturaServidor(models.Model):
+    """(ArquitecturaServidor descripcion)"""
+    """ Modelo usado para almecenar los posibles tipos de arquitecturas del los
+    posibles servidores del cliente para uso comercial """
+    Nombre = models.CharField(blank=True, max_length=100)
+
+    class Admin:
+        list_display = ('Nombre',)
+        search_fields = ('Nombre',)
+
+    def __unicode__(self):
+        return u"Arquitectura %s" % (self.Nombre)
+
+
+class TipoDb(models.Model):
+    """(TipoDB descripcion)"""
+    """ modelo usado para almacenar los posibles tipos de bases de datos que
+    que emplea el cliente en su servidor , informacion usada para uso comercial
+    """
+    Nombre = models.CharField(blank=True, max_length=100)
+
+    class Admin:
+        list_display = ('Nombre',)
+        search_fields = ('Nombre',)
+
+    def __unicode__(self):
+        return u"Base de datos tipo %s" % (self.Nombre)
+
+
+class TipoClusterdb(models.Model):
+    """(TipoClusterdb description)"""
+    """ Modelo usado para almacenar los posibles tipos de cluster que usan las
+    bases de datos que el cliente usa, informacion almacenada para uso
+    comercial """
+    Nombre = models.CharField(blank=True, max_length=100)
+
+    class Admin:
+        list_display = ('Nombre',)
+        search_fields = ('Nombre',)
+
+    def __unicode__(self):
+        return u"Tipo Cluster %s" % (slef.Nombre)
+
+
+class VersionDB(models.Model):
+    """(VersionDB description)"""
+    """modelo usado para almacenar de manera relacionada las la version de Base
+    de datos usada por el cliente"""
+    Version = models.CharField(blank=True, max_length=100)
+    Tipo_Db = models.ForeignKey(TipoDb)
+
+    class Admin:
+        list_display = ('Version', 'Tipo_Db')
+        search_fields = ('Version',)
+
+    def __unicode__(self):
+        return u"Version %s" % (self.version + " - " + self.Tipo_Db)
+
+
+class TipoAplicacion(models.Model):
+    """(TipoAplicacion description)"""
+    """ Modelo usado para almacenar los tipos de servidores de aplicacion que
+    el cliente quiere o tiene, informacion almacenada para uso comercial """
+    Nombre = models.CharField(blank=True, max_length=100)
+
+    class Admin:
+        list_display = ('Nombre',)
+        search_fields = ('Nombre',)
+
+    def __unicode__(self):
+        return u"Tipo Servidor Aplicacion %s" % (self.Nombre)
+
+
+class EsquemaLicenciamiento(models.Model):
+    """(EsquemaLicenciamiento description)"""
+    """ Modelo usado para almacenar el esquema de licenciamiento de un servidor
+    de aplicacion o base de datos que usa el cliente o quiere usar, informacion
+    almacenada para uso comercial """
+    Nombre = models.CharField(blank=True, max_length=100)
+
+    class Admin:
+        list_display = ('Nombre',)
+        search_fields = ('Nombre',)
+
+    def __unicode__(self):
+        return u" %s " % (self.Nombre)
+
+
+class ServidorEntidad(models.Model):
+    """(ServidorEntidad description)"""
+    """ Modelo usado para almacenar de manera realacionda informacion que el
+    cliente proporcione de sus servidores, informacion almacenada para posible
+    uso comercial """
+    Cliente = models.ForeignKey(ClienteEntidad)
+    Rol = models.ForeignKey(RolServidor)
+    Tipo = models.CharField(blank=True, max_length=100)
+    Virtualizador = models.ForeignKey(VirtualizadorServidor)
+    Sistema_Operativo = models.ForeignKey(SistemaOperativo)
+    Arquitectura = models.ForeignKey(ArquitecturaServidor)
+    No_Nucleos_CPU = models.IntegerField(blank=True, null=True)
+    Uso_CPU = models.FloatField(blank=True, null=True)
+    Ram = models.IntegerField(blank=True, null=True)
+    Uso_Ram = models.FloatField(blank=True, null=True)
+    Uso_Disco = models.IntegerField(blank=True, null=True)
+
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"Servidor"
